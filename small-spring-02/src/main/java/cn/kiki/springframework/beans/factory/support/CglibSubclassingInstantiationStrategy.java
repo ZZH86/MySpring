da34eb2c222522b2ef3bb360068c322ef2d5fa71
caohui <1518935897@qq.com>
2023-10-11 21:28:33 +0800

    Cglib进行带参数实例化

---
+++ small-spring-02/src/main/java/cn/kiki/springframework/beans/factory/support/CglibSubclassingInstantiationStrategy.java
@@ -0,0 +1,29 @@
+package cn.kiki.springframework.beans.factory.support;
+
+import cn.kiki.springframework.beans.BeansException;
+import cn.kiki.springframework.beans.factory.config.BeanDefinition;
+import net.sf.cglib.proxy.Enhancer;
+import net.sf.cglib.proxy.NoOp;
+
+import java.lang.reflect.Constructor;
+
+/**
+ * @Author hui cao
+ * @Description: Cglib 实例化
+ */
+public class CglibSubclassingInstantiationStrategy implements InstantiationStrategy{
+
+    @Override
+    public Object instantiate(BeanDefinition beanDefinition, String beanName, Constructor ctor, Object[] args) throws BeansException {
+        Enhancer enhancer = new Enhancer();
+        enhancer.setSuperclass(beanDefinition.getBeanClass());
+        enhancer.setCallback(new NoOp() {
+            @Override
+            public int hashCode() {
+                return super.hashCode();
+            }
+        });
+        if (null == ctor) return enhancer.create();
+        return enhancer.create(ctor.getParameterTypes(), args);
+    }
+}
