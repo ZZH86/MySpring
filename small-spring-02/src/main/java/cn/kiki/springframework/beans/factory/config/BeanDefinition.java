5d698768868e3df12f59ee79333430316312a19e
caohui <1518935897@qq.com>
2023-10-12 15:22:40 +0800

    bean 的引用

--- small-spring-02/src/main/java/cn/kiki/springframework/beans/factory/config/BeanDefinition.java
+++ small-spring-02/src/main/java/cn/kiki/springframework/beans/factory/config/BeanDefinition.java
@@ -1,5 +1,7 @@
 package cn.kiki.springframework.beans.factory.config;
 
+import cn.kiki.springframework.beans.PropertyValues;
+
 /**
  * @Author hui cao
  * @Description: bean 定义
@@ -7,11 +9,22 @@ package cn.kiki.springframework.beans.factory.config;
 public class BeanDefinition {
     private Class beanClass;
 
+    private PropertyValues propertyValues;
+
     public BeanDefinition(Class beanClass){
         this.beanClass = beanClass;
     }
 
+    public BeanDefinition(Class beanClass, PropertyValues propertyValues) {
+        this.beanClass = beanClass;
+        this.propertyValues = propertyValues != null ? propertyValues : new PropertyValues();
+    }
+
     public Class getBeanClass() {
         return beanClass;
+    }
+
+    public PropertyValues getPropertyValues() {
+        return propertyValues;
     }
 }
