efaa1b42857ccf020cc3fc8cdde767ad061fcada
caohui <1518935897@qq.com>
2023-11-12 21:39:54 +0800

    双检加锁实现单例

--- kiki-spring/src/main/java/cn/kiki/springframework/beans/factory/support/DefaultSingletonBeanRegistry.java
+++ kiki-spring/src/main/java/cn/kiki/springframework/beans/factory/support/DefaultSingletonBeanRegistry.java
@@ -2,6 +2,7 @@ package cn.kiki.springframework.beans.factory.support;
 
 import cn.kiki.springframework.beans.BeansException;
 import cn.kiki.springframework.beans.factory.DisposableBean;
+import cn.kiki.springframework.beans.factory.ObjectFactory;
 import cn.kiki.springframework.beans.factory.config.BeanPostProcessor;
 import cn.kiki.springframework.beans.factory.config.SingletonBeanRegistry;
 
@@ -19,13 +20,48 @@ public abstract class DefaultSingletonBeanRegistry implements SingletonBeanRegis
 
     protected static final Object NULL_OBJECT = new Object();
 
+    // 一级缓存，普通对象
     private final ConcurrentHashMap<String,Object> concurrentHashMap= new ConcurrentHashMap<>();
+
+    // 二级缓存，提前暴漏对象，没有完全实例化的对象
+    protected final Map<String, Object> earlySingletonObjects = new HashMap<String, Object>();
+
+    // 三级缓存，存放代理对象
+    private final Map<String, ObjectFactory<?>> singletonFactories = new HashMap<String, ObjectFactory<?>>();
 
     private final Map<String, DisposableBean> disposableBeans = new LinkedHashMap<>();
 
     @Override
     public Object getSingleton(String beanName) {
-        return concurrentHashMap.get(beanName);
+        // 双检加锁实现单例
+        Object singletonObject = concurrentHashMap.get(beanName);
+        if(null == singletonObject){
+            synchronized(this){
+                singletonObject = concurrentHashMap.get(beanName);
+                if(null == singletonObject){
+                    singletonObject = earlySingletonObjects.get(beanName);
+                    // 判断二级缓存中是否有对象，这个对象就是代理对象，因为只有代理对象才会放到三级缓存中
+                    if(null == singletonObject){
+                        ObjectFactory<?> singletonFactory = singletonFactories.get(beanName);
+                        if(singletonFactory != null){
+                            singletonObject = singletonFactory.getObject();
+                            // 把三级缓存中的代理对象中的真实对象获取出来，放入二级缓存中
+                            earlySingletonObjects.put(beanName, singletonObject);
+                            singletonFactories.remove(beanName);
+                        }
+                    }
+                }
+            }
+        }
+
+        return singletonObject;
+    }
+
+    protected void addSingletonFactory(String beanName, ObjectFactory<?> singletonFactory){
+        if (!this.concurrentHashMap.containsKey(beanName)) {
+            this.singletonFactories.put(beanName, singletonFactory);
+            this.earlySingletonObjects.remove(beanName);
+        }
     }
 
     /**
@@ -41,6 +77,8 @@ public abstract class DefaultSingletonBeanRegistry implements SingletonBeanRegis
     @Override
     public void registerSingleton(String beanName, Object singletonObject) {
         concurrentHashMap.put(beanName,singletonObject);
+        earlySingletonObjects.remove(beanName);
+        singletonFactories.remove(beanName);
     }
 
     //    public abstract void addBeanPostProcessor(BeanPostProcessor beanPostProcessor);
